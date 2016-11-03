8b3e6a50c5e7d14fe66b50f966ee5f16244e117c
michaeldumalag <michaeldumalag@gmail.com>
2016-11-03 16:04:01 -0700

    Add test to check if data matches what is displayed on landing

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,9 +1,20 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const React = require('react')
+const Search = require('../js/Search')
+const ShowCard = require('../js/ShowCard')
+const { shallow } = require('enzyme')
+const { shows } = require('../public/data')
 
 describe('<Search />', () => {
-  it('should pass', () => {
-    expect(1 + 1).to.equal(2)
+  it('should render the brand', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
+  })
+
+  it('should render as many shows as there are data for', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(ShowCard).length).to.equal(shows.length)
   })
 })
