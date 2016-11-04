f90a8f271b29b9d4851e8d004bbb6d81960a9c3b
michaeldumalag <michaeldumalag@gmail.com>
2016-11-04 16:18:15 -0700

    Create details route to match the object

--- js/Details.jsx
+++ js/Details.jsx
@@ -4,7 +4,9 @@ class Details extends React.Component {
   render() {
     return (
       <div style={{textAlign: 'left'}} className='container'>
- 
+        <pre><code>
+          {JSON.stringify(this.props.params, null, 4)}
+        </code></pre>
       </div>
     )
   }
