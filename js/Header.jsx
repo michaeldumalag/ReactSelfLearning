a6419f941338ff6a3dc7fa93f00da758f2810409
michaeldumalag <michaeldumalag@gmail.com>
2016-11-06 20:20:51 -0800

    Setup Search to pass correct props to Header component

--- js/Header.jsx
+++ js/Header.jsx
@@ -1,8 +1,29 @@
 const React = require('react')
 const { Link } = require('react-router')
+const { func, bool, string } = React.PropTypes
 
 const Header = React.createClass({
+  propTypes: {
+    handleSearchTermChange: func,
+    showSearch: bool,
+    searchTerm: string
+  },
+  handleSearchTermEvent (event) {
+    this.props.handleSearchTermChange(event.target.value)
+  },
   render () {
+    let utilSpace
+    if (this.props.showSearch) {
+      utilSpace = <input value={this.props.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+    } else {
+      utilSpace = (
+        <h2 className='header-back'>
+          <Link to='/search'>
+            Back
+          </Link>
+        </h2>
+      )
+    }
     return (
       <header className='header'>
         <h1 className='brand'>
@@ -10,6 +31,7 @@ const Header = React.createClass({
             svideo
           </Link>
         </h1>
+        {utilSpace}
       </header>
     )
   }
