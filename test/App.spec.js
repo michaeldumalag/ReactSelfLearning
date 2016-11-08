428fc92961fffa5ad186b1238d96637fbbe046ad
michaeldumalag <michaeldumalag@gmail.com>
2016-11-07 21:36:01 -0800

    add second rootReducer test

--- test/App.spec.js
+++ test/App.spec.js
@@ -8,7 +8,7 @@ const { shallow, mount } = require('enzyme')
 const { shows } = require('../public/data')
 const { store, rootReducer } = require('../js/Store')
 
-describe('<Search />', () => {
+xdescribe('<Search />', () => {
   it('should render the brand', () => {
     const wrapper = shallow(<Search />)
     expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
@@ -33,6 +33,10 @@ describe('Store', () => {
   it('should bootstrap', () => {
     const state = rootReducer(undefined, { type: '@@redux/INIT' })
     expect(state).to.deep.equal({ searchTerm: '' })
+  })
+  it('should handle setSearchTerm actions', () => {
+    const state = rootReducer({ searchTerm: 'some random string'}, { type: 'setSearchTerm', value: 'correct string'})
+    expect(state).to.deep.equal({ searchTerm: 'correct string'})
   })
 })
 
